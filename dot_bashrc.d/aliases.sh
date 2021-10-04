67338c03dca554e26c9c3b38597f2f7899026446
pakueamb <pakueamb@gmail.com>
2021-10-04 17:15:47 -0400

    Added a chezmoi alias

--- dot_bashrc.d/aliases.sh
+++ dot_bashrc.d/aliases.sh
@@ -12,7 +12,9 @@ alias h="history"
 alias ..="cd .."                                                                
 alias ...="cd ../../.."                                                         
 alias ....="cd ../../../.."                                                     
-                                                                                
+
+alias mkdir="mkdir -vp"
+
 alias hg="history | grep"                                                       
 alias df="pydf"                                                                 
                                                                                 
@@ -22,13 +24,15 @@ alias updatey="sudo dnf update -y"
 alias rm='rm -iv'
 alias rmy='rm -v'
 
+alias vim='nvim'
+
 # in case I need to manually fix the prompt format
 alias ps1fix='export PS1="\[\e[35m\][\[\e[m\]\[\e[35m\]\A\[\e[m\]\[\e[35m\]]\[\e[m\]\[\e[35m\] \[\e[m\]\[\e[35m\]\W\[\e[m\]\[\e[35m\] \[\e[m\]\[\e[35m\]\\$\[\e[m\] "; clear'
 
+# chezmoi aliases
 alias cz='chezmoi'
 alias cza='chezmoi re-add'
 alias czm='chezmoi managed'
 alias czg='chezmoi git'
 alias czd='chezmoi diff'
-
-alias vim='nvim'
+alias czcd='chezmoi cd'
