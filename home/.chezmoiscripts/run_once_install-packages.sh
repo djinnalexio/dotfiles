82a2c55b1d88155e7afb120066a1eed14b0ce7e1
Philippe-André Akue <pakueamb@gmail.com>
2023-03-03 16:18:35 +0100

    correction

--- home/.chezmoiscripts/run_once_install-packages.sh
+++ home/.chezmoiscripts/run_once_install-packages.sh
@@ -5,6 +5,7 @@ echo "Setup dnf:"
 echo "fastestmirror=1" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
 echo "max_parallel_download=10" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
 echo "defaultyes=yes" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
+cat /etc/dnf/dnf.conf
 sudo dnf update -y --refresh
 
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
@@ -14,14 +15,17 @@ sudo dnf install autojump cmatrix git git-lfs lsd man neofetch neovim tldr unzip
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "Installing nerd fonts and symbols:"
 for font in NerdFontsSymbolsOnly CascadiaCode CodeNewRoman FantasqueSansMono FiraCode JetBrainsMono Meslo SourceCodePro Terminus Ubuntu VictorMono
-    if [ ! -d "~/.local/share/fonts/$font" ] ; then
-    do
+do
+    if [[ ! -d "$HOME/.local/share/fonts/$font" ]] ; then
         wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/$font.zip" --directory-prefix ~/.local/share/fonts
         unzip ~/.local/share/fonts/$font.zip -d ~/.local/share/fonts/$font/ &> /dev/null
         rm ~/.local/share/fonts/$font.zip -f
+        rm ~/.local/share/fonts/$font/*Windows* -f
+        echo "Installed $font.\n"
+    else
+        echo "$font already installed.\n"
     fi
 done
-rm ~/.local/share/fonts/*/*Windows* -f
 
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "Installing vim-plug and setting up plugins for neovim:"
