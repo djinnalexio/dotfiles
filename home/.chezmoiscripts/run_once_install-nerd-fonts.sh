97fdb03a6ff6097d443ffcfd6abb9b3d0a30a9df
djinnalexio <pakueamb@gmail.com>
2024-04-01 17:07:20 +0100

    ✨ Added install scripts for ubuntu

---
+++ home/.chezmoiscripts/run_once_install-nerd-fonts.sh
@@ -0,0 +1,16 @@
+printf '%.s-' {1..$COLUMNS}
+echo "Installing nerd fonts and symbols:"
+for font in NerdFontsSymbolsOnly CascadiaCode CodeNewRoman FantasqueSansMono FiraCode JetBrainsMono Meslo SourceCodePro Terminus Ubuntu VictorMono
+do
+    if [[ ! -d "$HOME/.local/share/fonts/$font" ]] ; then
+        wget "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font.zip" --directory-prefix ~/.local/share/fonts
+        unzip ~/.local/share/fonts/$font.zip -d ~/.local/share/fonts/$font/ &> /dev/null
+        rm ~/.local/share/fonts/$font.zip -f
+        rm ~/.local/share/fonts/$font/*Windows* -f
+        echo "Installed $font."
+        echo
+    else
+        echo "$font already installed."
+        echo
+    fi
+done
