16fbdf918bcc485430a7048fcd654045b743724b
djinnalexio <pakueamb@gmail.com>
2024-04-02 17:33:09 +0100

    refactor: :recycle: combined post install scripts in one

--- home/.chezmoiscripts/run_once_after_post-install.sh
+++ home/.chezmoiscripts/run_once_after_post-install.sh
@@ -21,3 +21,16 @@ done
 
 printf "%.s-" $(seq 1 80)
 echo
+echo "Installing vim-plug and setting up plugins for neovim:"
+if [ ! -f "$HOME/.local/share/nvim/site/autoload/plug.vim" ] ; then
+    wget "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" --directory-prefix ~/.local/share/nvim/site/autoload/
+fi
+nvim -c ':PlugInstall' -c ':qa'
+
+printf "%.s-" $(seq 1 80)
+echo
+echo "Setting up ZSH as the default shell:"
+chsh -s /usr/bin/zsh
+
+printf "%.s-" $(seq 1 80)
+echo
