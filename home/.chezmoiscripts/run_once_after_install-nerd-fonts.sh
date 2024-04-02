8a69f0d9e8562bf7cfe83ef6a99b621f1f2e8ed3
djinnalexio <pakueamb@gmail.com>
2024-04-02 12:28:25 +0100

    fix: 🐛 fixed if statements on ubuntu

--- home/.chezmoiscripts/run_once_after_install-nerd-fonts.sh
+++ home/.chezmoiscripts/run_once_after_install-nerd-fonts.sh
@@ -6,9 +6,9 @@ echo "Installing nerd fonts and symbols:"
 echo
 for font in NerdFontsSymbolsOnly CascadiaCode CodeNewRoman FantasqueSansMono FiraCode JetBrainsMono Meslo SourceCodePro Terminus Ubuntu VictorMono
 do
-    if [[ ! -d "$HOME/.local/share/fonts/$font" ]] ; then
+    if [ ! -d "$HOME/.local/share/fonts/$font" ] ; then
         wget "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font.zip" --directory-prefix ~/.local/share/fonts
-        unzip ~/.local/share/fonts/$font.zip -d ~/.local/share/fonts/$font/ &> /dev/null
+        unzip ~/.local/share/fonts/$font.zip -d ~/.local/share/fonts/$font/ > /dev/null
         rm ~/.local/share/fonts/$font.zip -f
         rm ~/.local/share/fonts/$font/*Windows* -f
         echo "Installed $font."
--- home/.chezmoiscripts/run_once_after_install-vim-plugins.sh
+++ home/.chezmoiscripts/run_once_after_install-vim-plugins.sh
@@ -3,7 +3,7 @@
 printf "%.s-" $(seq 1 80)
 echo
 echo "Installing vim-plug and setting up plugins for neovim:"
-if [[ ! -f "$HOME/.local/share/nvim/site/autoload/plug.vim" ]] ; then
+if [ ! -f "$HOME/.local/share/nvim/site/autoload/plug.vim" ] ; then
     wget "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" --directory-prefix ~/.local/share/nvim/site/autoload/
 fi
 nvim -c ':PlugInstall' -c ':qa'
